f0202d108d8ef5dd0f82b1405e2c17ad57105378
chani1352 <chani1352@naver.com>
2024-12-19 17:44:51 +0900

    백신정보 update

--- backendTest/src/main/java/com/pnu/util/CustomMyUtil.java
+++ backendTest/src/main/java/com/pnu/util/CustomMyUtil.java
@@ -1,26 +1,36 @@
 package com.pnu.util;
 
+import java.util.Map;
+
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 public class CustomMyUtil {
 	
 	public static String getUsernameFromOAuth2User(OAuth2User user) {
 		System.out.println("CustomMyUtil getUsernameFromOAuth2User"); //확인용
-		
 		String userString = user.toString();
 		String regName = null;
-		
-		if(userString.contains("google"))	regName = "Google";
-		else if(userString.contains("naver"))	regName = "Naver";
-		else if(userString.contains("kakao"))	regName = "Kakao";
-		else {
-			if(userString.contains("id=") && userString.contains("resultcode=") && userString.contains("response="))
-				regName = "Naver";
-			else return null;
+		String name = null;
+		if(userString.contains("google")) {
+			regName = "Google";
+			name = user.getAttribute("email");
 		}
-		String name = user.getName();
+		else if(userString.contains("naver"))	{
+			regName = "Naver";
+			Map<String,String> info = user.getAttribute("response");
+			name = info.get("email");
+		}
+		else if(userString.contains("kakao"))	{
+			regName = "Kakao";
+			name = user.getAttribute("email");
+		}
+		else {
+			if(userString.contains("id=") && userString.contains("resultcode=") && userString.contains("response=")) {
+				regName = "Naver";
+				name = user.getAttribute("email");
+			} else return null;
+		}
 		if(name == null) return null;
-		
 		return regName + "_" + name;
 	}
 
