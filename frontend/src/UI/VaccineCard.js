a322ed0a0555fc5c3ce6f8a10f2d1fa14dfc4257
iambean-git <peb1021@naver.com>
2025-01-03 16:42:09 +0900

    250103 메인페이지 구성

--- frontend/src/UI/VaccineCard.js
+++ frontend/src/UI/VaccineCard.js
@@ -5,12 +5,19 @@ export default function VaccinesCard({ vaccine }) {
   //console.log("vaccine",vaccine);
   return (
     <div className="vacCard m-5">
-      <div className="">
-        <div className="px-6 flex flex-col justify-center items-start">
-          <div className="pt-5 text-xl font-bold">{vaccine["disease"]}</div>
-          <div className="pb-4 text-gray-700 text-sm">{vaccine["vaccineName"]} {vaccine["phase"]}</div>
-          <div className=""> {vaccine["method"]}</div>
-          <div className="pt-3 flex">
+      <div className="relative h-full">
+        <div className="p-6 flex flex-col justify-center items-start ">
+          <div className="text-xl font-bold font-NanumSquareR">{vaccine["disease"]}</div>
+          <div className="mt-1 text-gray-700 text-sm">{vaccine["vaccineName"]} {vaccine["phase"]}</div>
+          <div className="mt-6 flex items-center text-sm "> 
+            <div className="size-5 bg-gray-100 mr-1 rounded-full 
+                            flex justify-center items-center">
+              <img src="/img/flushot.png" className="size-4"></img>
+            </div>
+            <span className="mr-4">방법</span>
+            <span className="text-gray-700">{vaccine["method"]}</span>
+          </div>
+          <div className="flex absolute bottom-6">
             <CardInfoSmall text={"무료접종"} />
             <CardInfoSmall text={vaccine["optional"]} />
           </div>
